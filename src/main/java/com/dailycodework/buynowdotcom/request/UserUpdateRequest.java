df580ef3aa230056333e5427110329206335ecd1
hamodi15-code <mohamed.alfker4@gmail.com>
2026-01-03 11:45:45 +0200

    implement full CRUD for user entity

---
+++ src/main/java/com/dailycodework/buynowdotcom/request/UserUpdateRequest.java
@@ -0,0 +1,12 @@
+package com.dailycodework.buynowdotcom.request;
+
+
+import lombok.Data;
+
+@Data
+public class UserUpdateRequest {
+
+    private String firstName;
+    private String lastName;
+
+}
